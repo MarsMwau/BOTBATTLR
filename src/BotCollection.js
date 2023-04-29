aea3607191edb9545b6ba3547852bb6856259e3c
MarsMwau <marthamumbua16@gmail.com>
2023-04-29 13:09:59 +0300

    Styling Bots

--- src/BotCollection.js
+++ src/BotCollection.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 function BotCollection({ botData, enlistBot }) {
   return (
     <div>
@@ -11,9 +10,11 @@ function BotCollection({ botData, enlistBot }) {
           <img src={bot.avatar_url} alt="Bot" />
           <h3>{bot.name}</h3>
           <h7>{bot.catchphrase}</h7>
-          <p><i className="fa fa-heartbeat" aria-hidden="true"></i> {bot.health}</p>
-          <p><i className="fa fa-bolt" aria-hidden="true"></i> {bot.damage}</p>
-          <p><i className="fa fa-shield" aria-hidden="true"></i> {bot.armor}</p>
+          <div className="stats">
+          <p><i className="fa fa-heartbeat" aria-hidden="true"></i>{bot.health } </p>
+          <p><i className="fa fa-bolt" aria-hidden="true"></i>{bot.damage } </p>
+          <p><i className="fa fa-shield" aria-hidden="true"></i>{bot.armor }</p>
+          </div>
           <button onClick={() => enlistBot(bot)}>Enlist</button>
         </div>
       ))}
